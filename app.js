f4b00a92c4a7c233096a68bfa9eefb6505f605b1
HaJaehee <lovesm135@naver.com>
2016-11-09 03:39:20 +0900

    EPCIS Access Control API version 1.1.1
    EPCIS Access Control API version 1.1.1
    added new feature: group access control.
    made its code fancy.
    "EPCIS" commit is same with this.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var cluster = require('cluster');
 if(cluster.isMaster) {
     var numWorkers = require('os').cpus().length;
 	
-	console.log('Master cluster setting up ' + numWorkers + ' workers...');
+	console.log( 'Master cluster setting up ' + numWorkers + ' workers...');
 	
 	for(var i = 0; i < numWorkers; i++) {
 	    cluster.fork();
